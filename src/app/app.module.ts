54117207a469c00d33c95fe6333d8fbfd3ac0c28
Leonardo Bustamante <leonardo.gomes@inatel.br>
2018-01-30 19:34:44 -0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
