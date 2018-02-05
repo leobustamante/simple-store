279470004a31a47ca2838d524bbe52acf65502c7
Leonardo Bustamante <leonardo.gomes@inatel.br>
2018-02-05 19:34:13 -0200

    css adjusts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,10 @@ import { GameDetailComponent } from './games/game-detail/game-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/games', pathMatch: 'full' },
-  { path: 'games', component: GamesComponent },
-  { path: 'game/:id', component: GameDetailComponent, data: {test: this.games} }
+  { path: 'games', component: GamesComponent, children:[
+    //{ path: ':id', component: GameDetailComponent, data: {test: this.games}}
+  ]},
+ { path: 'games/:id', component: GameDetailComponent, data: {test: this.games} }
 ];
 
 
